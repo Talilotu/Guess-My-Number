e03cad2f4821e92fc1cc27589f2ba6d59dc0683c
Talilotu <nesikaufusi@gmail.com>
2022-07-27 17:16:48 +1000

    set up high score

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ console.log(document.querySelector(".guess").value); //get or set the value
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value); // convert string to a number
@@ -29,6 +30,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347"; // inline styles, value is a string
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
 
     //When guess is too high
   } else if (guess > secretNumber) {
