fb4da86801348c9c8e62cef78828741d74645017
Talilotu <nesikaufusi@gmail.com>
2022-07-27 17:51:58 +1000

    refactor same code

--- script.js
+++ script.js
@@ -36,20 +36,12 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".highscore").textContent = highscore;
     }
 
-    //When guess is too high
-  } else if (guess > secretNumber) {
+    //Refactoring codes to avoid DRY rule
+    //When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Too high";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "💥 You lost the game!";
-    }
-
-    //When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too low";
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "📈 Too high" : "📉 Too low";
       score--;
       document.querySelector(".score").textContent = score;
     } else {
@@ -57,8 +49,30 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
-});
 
+  //When guess is too high
+  // else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = "📈 Too high";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     document.querySelector(".message").textContent = "💥 You lost the game!";
+  //   }
+
+  //     //When guess is too low
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector(".message").textContent = "📉 Too low";
+  //       score--;
+  //       document.querySelector(".score").textContent = score;
+  //     } else {
+  //       document.querySelector(".message").textContent = "💥 You lost the game!";
+  //       document.querySelector(".score").textContent = 0;
+  //     }
+  //   }
+  // });
+});
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
