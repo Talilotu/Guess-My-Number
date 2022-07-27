3850333c16646f9248930c4225fb2f36fa4dd08a
Talilotu <nesikaufusi@gmail.com>
2022-07-27 16:51:36 +1000

    set reset click handler

--- script.js
+++ script.js
@@ -29,6 +29,14 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347"; // inline styles, value is a string
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".again").addEventListener("click", function () {
+      document.querySelector(".number").textContent = "?";
+      document.querySelector(".score").textContent = 20;
+      document.querySelector(".guess").value = 0;
+      document.querySelector(".message").textContent = "Start guessing...";
+      document.querySelector("body").style.backgroundColor = "#333";
+      document.querySelector(".number").style.width = "15rem";
+    });
 
     //When guess is too high
   } else if (guess > secretNumber) {
@@ -38,7 +46,6 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = "💥 You lost the game!";
-      document.querySelector(".score").textContent = 0;
     }
 
     //When guess is too low
