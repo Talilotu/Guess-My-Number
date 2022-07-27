3dcd2a123c9e7b0240270c18f656eb2dab8f2771
Talilotu <nesikaufusi@gmail.com>
2022-07-27 17:03:38 +1000

    updated again event handler

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value); //get or set the value
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -29,14 +29,6 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347"; // inline styles, value is a string
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
-    document.querySelector(".again").addEventListener("click", function () {
-      document.querySelector(".number").textContent = "?";
-      document.querySelector(".score").textContent = 20;
-      document.querySelector(".guess").value = 0;
-      document.querySelector(".message").textContent = "Start guessing...";
-      document.querySelector("body").style.backgroundColor = "#333";
-      document.querySelector(".number").style.width = "15rem";
-    });
 
     //When guess is too high
   } else if (guess > secretNumber) {
@@ -59,4 +51,16 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#333";
+  document.querySelector(".number").style.width = "15rem";
 });
